194e948422540cd3db5a4bce0c7f9daa85c90710
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-10-12 14:51:21 -0300

    Updated version

--- src/com/maxprograms/keysanalyzer/Constants.java
+++ src/com/maxprograms/keysanalyzer/Constants.java
@@ -2,9 +2,9 @@ package com.maxprograms.keysanalyzer;
 
 public class Constants {
 
-	public static final String VERSION = "1.0-2";
-	public static final String BUILD = "7E1-2-A";
-	public static final String PREFERENCES = "KeyAnalizer.pref";
+	public static final String VERSION = "1.0-3";
+	public static final String BUILD = "20191012_1436";
+	public static final String PREFERENCES = "KeysAnalizer.pref";
 	public static final String CATALOG = "catalog/catalog.xml";
 
 }
