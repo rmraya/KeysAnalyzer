1ab2345f945a2f88c4602d4bad2eb1409ecf41a1
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-10-12 18:30:09 -0300

    Removed unused jars

--- keysanalyzer.bat
+++ keysanalyzer.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CLASSPATH="lib\keysanalyzer.jar;lib\dtd.jar;lib\json.jar;lib\jsoup-1.11.3.jar;lib\mapdb.jar;lib\openxliff.jar;lib\win64\swt.jar"
+set CLASSPATH="lib\keysanalyzer.jar;lib\mapdb.jar;lib\openxliff.jar;lib\win64\swt.jar"
 
 start javaw.exe  -cp %CLASSPATH% com.maxprograms.keysanalyzer.KeysAnalyzer
 
