90b53cb4a0068ac0c149a9495bb14674c52b3183
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-10-12 18:54:44 -0300

    Updated build number

--- src/com/maxprograms/keysanalyzer/Constants.java
+++ src/com/maxprograms/keysanalyzer/Constants.java
@@ -26,7 +26,7 @@ package com.maxprograms.keysanalyzer;
 public class Constants {
 
 	public static final String VERSION = "1.0-3";
-	public static final String BUILD = "20191012_1609";
+	public static final String BUILD = "20191012_1853";
 	public static final String PREFERENCES = "KeysAnalizer.pref";
 
 }
